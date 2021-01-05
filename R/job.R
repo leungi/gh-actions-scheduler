9b23c6a6a97c48b18253a5d3e06504ee4efc06ed
leungi <Ivan_leung@oxy.com>
2021-01-05 01:37:50 -0600

    :tada:

---
+++ R/job.R
@@ -0,0 +1,13 @@
+#' Generate random numbers and save to file
+#'
+#' @return Saved file in ./data_raw
+#' @export
+#'
+#' @examples
+#' cron_job()
+library(tictoc)
+
+job <- function() {
+  x <- rnorm(1:10)
+  save(x, file = paste0("data_raw/data_", make.names(Sys.time()), ".Rda"))
+}
